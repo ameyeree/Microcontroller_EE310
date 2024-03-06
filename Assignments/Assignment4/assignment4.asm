6f7072df6c427cac8b7c2903f545194584904aea
ameyeree <161140957+ameyeree@users.noreply.github.com>
2024-03-06 08:18:26 -0800

    Finalized
    Comments updated, as well as correcting some minor issues

--- Assignments/Assignment4/assignment4.asm
+++ Assignments/Assignment4/assignment4.asm
@@ -2,24 +2,31 @@
 ; Title: Assignment 4
 ;---------------------
 ;Program Details:
-; The purpose of this program is to load two bytes, Byte1 (F2H) and Byte2 (02H), in data registers REG0 
-; The program may contain logical errors and should be debugged. Follow the comments and correct the errors
-
-;-------
-; Read This:
-;-------
-; You may need to change the path for the AssemblyConfig.inc; this file contains all the configuration bits
-; To simulate go to Windows tab, then Target Memory View--> File Registers and view teh registers
-
-		
-; Inputs: Byte 1 and Byte 2
-; Outputs: Register o
+; Receives a measured value (currently hard coded), and a reference value
+; (also currently hard coded) for the temperature in Celsius. 
+; Stores these values in decimal form in specified registers (3 for each).
+; If a value is negative, will store 0x01 in the "hundreds" position.
+; Compares the values and turns HEATER or COOLER off depending on measurements:
+; -If the temperatures are equal, will set HEATER and COOLER to zero (off),
+; and controller register to 0.
+; -If the measured > reference, will set HEATER to one (on) and COOLER to
+; zero (off), and controller register to 1.
+; -If the measured > reference, will set HEATER to zero (off) and COOLER to
+; one (on), and controller register to 2.
+; Program Constraints: 
+;   measured temp: between -20 and +60 degrees Celsius
+;   reference temp: between +10 and +50 degrees Celsius
+;   only using PORTD
+;   must start from register 0x20				    
+				    
+; Inputs: measuredTempInput, refTempInput
+; Outputs: HEATER (RD1), COOLER (RD2)
 ; Date: March 4th, 2024
 ; File Dependencies / Libraries: None 
-; Compiler: ASM 2.4
+; Compiler: ASM 2.46
 ; Author: Anthony MeyerSlechta
 ; Versions:
-;       V1.0: Oriiginal Program
+;       V1.0: Original Program
 
 ;---------------------
 ; Initialization
@@ -30,35 +37,37 @@
 ;---------------------
 ; Program Inputs
 ;---------------------
-#define	measuredTempInput   -20   ;Input value for temp sensor
-#define refTempInput	    20	 ;Input value for keypad (user value)
+#define	measuredTempInput   -5	    ;Input value for temp sensor
+				    ;Value is assumed to be between -20 and 60
+#define refTempInput	    15	    ;Input value for keypad (user value)
+				    ;Value is assumed to be between 10 and 50
     
 ;---------------------
 ; Registers
 ;--------------------- 
-#define refTemp	   	    0x20    ;
-#define measuredTemp	    0x21    ;
-#define contReg		    0x22    ;
-#define measuredDecHund	    0x72    ;
-#define measuredDecTens	    0x71    ;
-#define measuredDecOnes	    0x70    ;
-#define refDecHund	    0x62    ;
-#define refDecTens	    0x61    ;
-#define refDecOnes	    0x60    ;    
-numerator   EQU	    0x30    ;For decimal conversion
-denominator EQU	    0x31    ;For decimal conversion
-quotient    EQU	    0x33    ;For decimal conversion
+refTemp		EQU	    0x20    ;Reference Temp given by user
+measuredTemp	EQU	    0x21    ;Measured temp "given by temp sensor"				    
+contReg		EQU	    0x22    ;Controller register
+measuredDecHund	EQU	    0x72    ;Convert to decimal (hundreds position)
+measuredDecTens	EQU	    0x71    ;Convert to decimal (tens position)
+measuredDecOnes EQU	    0x70    ;Convert to decimal (ones position)
+refDecHund	EQU	    0x62    ;Convert to decimal (hundreds position)
+refDecTens	EQU	    0x61    ;Convert to decimal (tens position)
+refDecOnes	EQU	    0x60    ;Convert to decimal (ones position)
+numerator	EQU	    0x30    ;For decimal conversion
+denominator	EQU	    0x31    ;For decimal conversion
+quotient	EQU	    0x33    ;For decimal conversion
     
 ;---------------------
 ; Program Outputs
 ;---------------------
-#define	HEATER	PORTD,1
-#define COOLER	PORTD,2
+#define	HEATER	PORTD,1		    ;PORT D1
+#define COOLER	PORTD,2		    ;PORT D2
 
 ;---------------------
 ; Program Constants
 ;---------------------
-#define	convDenom	10	    ;Used for dividing to convert
+#define	convDenom	10	    ;Used for dividing to convert to decimal
     
 ;---------------------
 ; Main Program
@@ -70,54 +79,68 @@ quotient    EQU	    0x33    ;For decimal conversion
 
     ORG          0x20		    ;Begin assembly at 0x20
     
-    
-_start1:
+_start1:			    ;Initializes PORTD
     MOVLW   0x00
     MOVWF   TRISD,0
     MOVWF   PORTD,0
-
+    
+_measuredTempInput:
     MOVLW   measuredTempInput
     MOVWF   measuredTemp
-    CALL    _convertMeasured
+    BTFSS   measuredTemp,7	    ;If negative,convert to "magnitude" of number
+    GOTO    _convMCall		
+    NEGF    WREG
     
+_convMCall:			    ;Convert measured temp to decimal and save
+    CALL    _convertMeasured	    ; in registers 0x70,0x71,0x72
+    MOVLW   measuredTempInput
+    
+_referenceTempInput:
+    MOVLW   refTempInput	    
+    MOVWF   refTemp	  
+    
+_convRCall:			    ;Convert reference temp to decimal and save
+    CALL    _convertReference	    ; in registers 0x60,0x61,0x62
     MOVLW   refTempInput
-    MOVWF   refTemp	    
-    CALL    _convertReference
+    
+_isNegative:			;Assuming ref temp can not be negative
+    BTFSC   measuredTemp,7	; if measuredTemp is negative, then 
+    GOTO    _heater		; go to heater
     
 _compare:
-    CPFSGT  measuredTemp,a	;if measuredTemp > refTemp go to _greater
-    CPFSLT  measuredTemp,a	;if measuredTemp < refTemp go to _less
-    BRA	    _greater
-    CPFSEQ  measuredTemp,a	;if measuredTemp == refTemp go to equal
-    BRA	    _less
-    BRA	    _equal
-    
-_equal:			    ;if measuredTemp == refTemp go to equal
-    MOVLW   0x00	    ;To set contReg to 0
+    CPFSGT  measuredTemp,a	;if measuredTemp > refTemp go to cooler
+    CPFSLT  measuredTemp,a	;if measuredTemp < refTemp go to heater
+    BRA	    _cooler
+    CPFSEQ  measuredTemp,a	;if measuredTemp == refTemp go to off
+    BRA	    _heater
+    BRA	    _off
+  
+_off:				;if measuredTemp == refTemp go to off
+    MOVLW   0x00		;To set contReg to 0
     BCF	    HEATER
     BCF	    COOLER
     BRA	    _end
     
-_greater:		    ;if measuredTemp > refTemp go to _greater   
-    MOVLW   0x02	    ;To set contReg to 2
+_cooler:			;if measuredTemp > refTemp go to cooler   
+    MOVLW   0x02		;To set contReg to 2
     BCF	    HEATER
     BSF	    COOLER
     BRA	    _end
     
-_less:			    ;if measuredTemp < refTemp go to _less
-    MOVLW   0x01	    ;To set contReg to 1
+_heater:			;if measuredTemp < refTemp go to heater
+    MOVLW   0x01		;To set contReg to 1
     BSF	    HEATER
     BCF	    COOLER
     BRA	    _end
     
     
-_convertMeasured: ;Convert to decimal: Code taken from Mazid Pg 165
-    ORG	    0x300
+_convertMeasured:	    ;Convert to decimal: Code taken from Mazid Pg 165
+    ORG	    0x300	    ; Uses division (in the form of subtraction)
     
     MOVWF   numerator
     MOVLW   convDenom
     CLRF    quotient
-_digit1m:
+_digit1m:				;Converts hex to decimal (in ones pos)
     INCF    quotient,F
     SUBWF   numerator
     BC	    _digit1m
@@ -126,14 +149,18 @@ _digit1m:
     MOVFF   numerator,measuredDecOnes
     MOVFF   quotient,numerator
     CLRF    quotient
-_digit2m:
+_digit2m:				;Converts hex to decimal (in tens pos)
     INCF    quotient,F
     SUBWF   numerator
     BC	    _digit2m
     ADDWF   numerator
     DECF    quotient,F
     MOVFF   numerator,measuredDecTens
-    MOVFF   quotient,measuredDecHund
+    BTFSS   measuredTemp,7		;If negative, store 0x01 in hundreds pos
+    RETURN
+    MOVLW   0x01
+    MOVWF   measuredDecHund
+    ;MOVFF   quotient,measuredDecHund	;Code used for if decimal > 99
     RETURN
     
 _convertReference: ;Convert to decimal: Code taken from Mazid Pg 165
@@ -142,7 +169,7 @@ _convertReference: ;Convert to decimal: Code taken from Mazid Pg 165
     MOVWF   numerator
     MOVLW   convDenom
     CLRF    quotient
-_digit1r:
+_digit1r:			    ;Converts hex to decimal (in ones pos)
     INCF    quotient,F
     SUBWF   numerator
     BC	    _digit1r
@@ -151,15 +178,15 @@ _digit1r:
     MOVFF   numerator,refDecOnes
     MOVFF   quotient,numerator
     CLRF    quotient
-_digit2r:
-    INCF    quotient,F
+_digit2r:			    ;Converts hex to decimal 
+    INCF    quotient,F		    ; (in tens and hunds pos)
     SUBWF   numerator
     BC	    _digit2r
     ADDWF   numerator
     DECF    quotient,F
     MOVFF   numerator,refDecTens
-    MOVFF   quotient,refDecHund
-    RETURN
+    MOVFF   quotient,refDecHund	    ;Should not currently be used based on 
+    RETURN			    ; constraints
     
 _end:
     MOVWF   contReg,0
--- Assignments/Assignment4/build/default/debug/assignment4.i
+++ Assignments/Assignment4/build/default/debug/assignment4.i
@@ -5,24 +5,31 @@
 ; Title: Assignment 4
 ;---------------------
 ;Program Details:
-; The purpose of this program is to load two bytes, Byte1 (F2H) and Byte2 (02H), in data registers REG0
-; The program may contain logical errors and should be debugged. Follow the comments and correct the errors
+; Receives a measured value (currently hard coded), and a reference value
+; (also currently hard coded) for the temperature in Celsius.
+; Stores these values in decimal form in specified registers (3 for each).
+; If a value is negative, will store 0x01 in the "hundreds" position.
+; Compares the values and turns HEATER or COOLER off depending on measurements:
+; -If the temperatures are equal, will set HEATER and COOLER to zero (off),
+; and controller register to 0.
+; -If the measured > reference, will set HEATER to one (on) and COOLER to
+; zero (off), and controller register to 1.
+; -If the measured > reference, will set HEATER to zero (off) and COOLER to
+; one (on), and controller register to 2.
+; Program Constraints:
+; measured temp: between -20 and +60 degrees Celsius
+; reference temp: between +10 and +50 degrees Celsius
+; only using PORTD
+; must start from register 0x20
 
-;-------
-; Read This:
-;-------
-; You may need to change the path for the AssemblyConfig.inc; this file contains all the configuration bits
-; To simulate go to Windows tab, then Target Memory View--> File Registers and view teh registers
-
-
-; Inputs: Byte 1 and Byte 2
-; Outputs: Register o
+; Inputs: measuredTempInput, refTempInput
+; Outputs: HEATER (RD1), COOLER (RD2)
 ; Date: March 4th, 2024
 ; File Dependencies / Libraries: None
-; Compiler: ASM 2.4
+; Compiler: ASM 2.46
 ; Author: Anthony MeyerSlechta
 ; Versions:
-; V1.0: Oriiginal Program
+; V1.0: Original Program
 
 ;---------------------
 ; Initialization
@@ -87,7 +94,7 @@
 
 
 ; config statements should precede project file includes.
-# 28 "assignment4.asm" 2
+# 35 "assignment4.asm" 2
 # 1 "C:\\Program Files\\Microchip\\xc8\\v2.46\\pic\\include\\xc.inc" 1 3
 
 
@@ -32751,18 +32758,28 @@ stk_offset SET 0
 auto_size SET 0
 ENDM
 # 5 "C:\\Program Files\\Microchip\\xc8\\v2.46\\pic\\include\\xc.inc" 2 3
-# 29 "assignment4.asm" 2
+# 36 "assignment4.asm" 2
 
 ;---------------------
 ; Program Inputs
 ;---------------------
 
+        ;Value is assumed to be between -20 and 60
 
+        ;Value is assumed to be between 10 and 50
 
 ;---------------------
 ; Registers
 ;---------------------
-# 48 "assignment4.asm"
+refTemp EQU 0x20 ;Reference Temp given by user
+measuredTemp EQU 0x21 ;Measured temp "given by temp sensor"
+contReg EQU 0x22 ;Controller register
+measuredDecHund EQU 0x72 ;Convert to decimal (hundreds position)
+measuredDecTens EQU 0x71 ;Convert to decimal (tens position)
+measuredDecOnes EQU 0x70 ;Convert to decimal (ones position)
+refDecHund EQU 0x62 ;Convert to decimal (hundreds position)
+refDecTens EQU 0x61 ;Convert to decimal (tens position)
+refDecOnes EQU 0x60 ;Convert to decimal (ones position)
 numerator EQU 0x30 ;For decimal conversion
 denominator EQU 0x31 ;For decimal conversion
 quotient EQU 0x33 ;For decimal conversion
@@ -32788,101 +32805,115 @@ quotient EQU 0x33 ;For decimal conversion
 
     ORG 0x20 ;Begin assembly at 0x20
 
-
-_start1:
+_start1: ;Initializes PORTD
     MOVLW 0x00
     MOVWF TRISD,0
     MOVWF PORTD,0
 
-    MOVLW -20 ;Input value for temp sensor
-    MOVWF 0x21 ;
-    BNN _sendMagnitudeM
+_measuredTempInput:
+    MOVLW -5 ;Input value for temp sensor
+    MOVWF measuredTemp
+    BTFSS measuredTemp,7 ;If negative,convert to "magnitude" of number
+    GOTO _convMCall
     NEGF WREG
 
-_sendMagnitudeM:
-    CALL _convertMeasured
+_convMCall: ;Convert measured temp to decimal and save
+    CALL _convertMeasured ; in registers 0x70,0x71,0x72
+    MOVLW -5 ;Input value for temp sensor
 
-    MOVLW 20 ;Input value for keypad (user value)
-    MOVWF 0x20 ;
-    CALL _convertReference
+_referenceTempInput:
+    MOVLW 15 ;Input value for keypad (user value)
+    MOVWF refTemp
+
+_convRCall: ;Convert reference temp to decimal and save
+    CALL _convertReference ; in registers 0x60,0x61,0x62
+    MOVLW 15 ;Input value for keypad (user value)
+
+_isNegative: ;Assuming ref temp can not be negative
+    BTFSC measuredTemp,7 ; if measuredTemp is negative, then
+    GOTO _heater ; go to heater
 
 _compare:
-    CPFSGT 0x21 ;,a ;if 0x21 ; > 0x20 ; go to _greater
-    CPFSLT 0x21 ;,a ;if 0x21 ; < 0x20 ; go to _less
-    BRA _greater
-    CPFSEQ 0x21 ;,a ;if 0x21 ; == 0x20 ; go to equal
-    BRA _less
-    BRA _equal
+    CPFSGT measuredTemp,a ;if measuredTemp > refTemp go to cooler
+    CPFSLT measuredTemp,a ;if measuredTemp < refTemp go to heater
+    BRA _cooler
+    CPFSEQ measuredTemp,a ;if measuredTemp == refTemp go to off
+    BRA _heater
+    BRA _off
 
-_equal:
-    MOVLW 0x00 ;To set 0x22 ; to 0
-    BCF PORTD,1
-    BCF PORTD,2
+_off: ;if measuredTemp == refTemp go to off
+    MOVLW 0x00 ;To set contReg to 0
+    BCF PORTD,1 ;PORT D1
+    BCF PORTD,2 ;PORT D2
     BRA _end
 
-_greater:
-    MOVLW 0x02 ;To set 0x22 ; to 2
-    BCF PORTD,1
-    BSF PORTD,2
+_cooler: ;if measuredTemp > refTemp go to cooler
+    MOVLW 0x02 ;To set contReg to 2
+    BCF PORTD,1 ;PORT D1
+    BSF PORTD,2 ;PORT D2
     BRA _end
 
-_less:
-    MOVLW 0x01 ;To set 0x22 ; to 1
-    BSF PORTD,1
-    BCF PORTD,2
+_heater: ;if measuredTemp < refTemp go to heater
+    MOVLW 0x01 ;To set contReg to 1
+    BSF PORTD,1 ;PORT D1
+    BCF PORTD,2 ;PORT D2
     BRA _end
 
 
 _convertMeasured: ;Convert to decimal: Code taken from Mazid Pg 165
-    ORG 0x300
+    ORG 0x300 ; Uses division (in the form of subtraction)
 
     MOVWF numerator
-    MOVLW 10 ;Used for dividing to convert
+    MOVLW 10 ;Used for dividing to convert to decimal
     CLRF quotient
-_digit1m:
+_digit1m: ;Converts hex to decimal (in ones pos)
     INCF quotient,F
     SUBWF numerator
     BC _digit1m
     ADDWF numerator
     DECF quotient,F
-    MOVFF numerator,0x70 ;
+    MOVFF numerator,measuredDecOnes
     MOVFF quotient,numerator
     CLRF quotient
-_digit2m:
+_digit2m: ;Converts hex to decimal (in tens pos)
     INCF quotient,F
     SUBWF numerator
     BC _digit2m
     ADDWF numerator
     DECF quotient,F
-    MOVFF numerator,0x71 ;
-    MOVFF quotient,0x72 ;
+    MOVFF numerator,measuredDecTens
+    BTFSS measuredTemp,7 ;If negative, store 0x01 in hundreds pos
+    RETURN
+    MOVLW 0x01
+    MOVWF measuredDecHund
+    ;MOVFF quotient,measuredDecHund ;Code used for if decimal > 99
     RETURN
 
 _convertReference: ;Convert to decimal: Code taken from Mazid Pg 165
     ORG 0x350
 
     MOVWF numerator
-    MOVLW 10 ;Used for dividing to convert
+    MOVLW 10 ;Used for dividing to convert to decimal
     CLRF quotient
-_digit1r:
+_digit1r: ;Converts hex to decimal (in ones pos)
     INCF quotient,F
     SUBWF numerator
     BC _digit1r
     ADDWF numerator
     DECF quotient,F
-    MOVFF numerator,0x60 ;
+    MOVFF numerator,refDecOnes
     MOVFF quotient,numerator
     CLRF quotient
-_digit2r:
-    INCF quotient,F
+_digit2r: ;Converts hex to decimal
+    INCF quotient,F ; (in tens and hunds pos)
     SUBWF numerator
     BC _digit2r
     ADDWF numerator
     DECF quotient,F
-    MOVFF numerator,0x61 ;
-    MOVFF quotient,0x62 ;
-    RETURN
+    MOVFF numerator,refDecTens
+    MOVFF quotient,refDecHund ;Should not currently be used based on
+    RETURN ; constraints
 
 _end:
-    MOVWF 0x22 ;,0
+    MOVWF contReg,0
 END
