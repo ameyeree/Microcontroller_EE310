53e02191fec642820f2da08d91550451235b3343
ameyeree <161140957+ameyeree@users.noreply.github.com>
2024-03-29 17:55:47 -0700

    Lab9

---
+++ Other/LAB9/main.c
@@ -0,0 +1,40 @@
+/*
+ * ---------------------
+ * Title: Simple LED Blinking 
+ * ---------------------
+ * Program Details:
+ *  The purpose of this program is to simply blink an LED 
+ * Inputs: myDelay 
+ * Outputs: RD0 (output)
+ * Setup: C- Simulator
+ * Date: Feb 24, 2023
+ * File Dependencies / Libraries: It is required to include the 
+ * Configuration Header File 
+ * Compiler: xc8, 2.4
+ * Author: Farid Farahmand
+ * Versions:
+ *      V1.0: Original
+ * Useful links:  
+ *      Datasheet: https://ww1.microchip.com/downloads/en/DeviceDoc/PIC18(L)F26-27-45-46-47-55-56-57K42-Data-Sheet-40001919G.pdf 
+ *      PIC18F Instruction Sets: https://onlinelibrary.wiley.com/doi/pdf/10.1002/9781119448457.app4 
+ *      List of Instrcutions: http://143.110.227.210/faridfarahmand/sonoma/courses/es310/resources/20140217124422790.pdf 
+ */
+
+#include <xc.h> // must have this
+//#include "../../../../../Program Files/Microchip/xc8/v2.40/pic/include/proc/pic18f46k42.h"
+#include "C:/Program Files/Microchip/xc8/v2.46/pic/include/proc/pic18f47k42.h"
+//#include "C:\Program Files\Microchip\xc8\v2.46\pic\include\proc\pic18f46k42.h"
+
+#include <stdio.h>
+#include <string.h>
+
+void main (void){
+    
+    
+    char buf1[50] = {"PIC18 is Amazing!"};
+    char buf2[50] = {"The IDE or the chip?"};
+        
+    strcpy(buf1,buf2);
+    LATB = 1;
+    
+}
